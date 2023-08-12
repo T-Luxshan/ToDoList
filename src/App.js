ff0f7f5463a68c45551586f146a54afa4b0bc8d8
T-Luxshan <luckybraveboys@gmail.com>
2023-08-12 18:56:53 +0530

    core logic finished

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
 import { useState } from 'react';
 import './App.css';
+import {Task} from './Task';
 
 function App() {
 
   const [toDoList, setToDoList] = useState([]); 
-  const [newTask, setNewTask] = useState(""); 
+  const [newTask, setNewTask] = useState("");
+  // const [completeColor, setcompleteColor] = useState("black");
 
   const handleChane = (event) => {
     setNewTask(event.target.value);
   };
 
   const addTask = () => {
-    // const newToDoList = [...toDoList, newTask]
-    setToDoList([...toDoList, newTask])
+    const task = {
+      id: toDoList.length === 0 ? 1 : toDoList[toDoList.length-1].id + 1,
+      taskName: newTask,
+      completed: false
+    }
+
+    setToDoList([...toDoList, task])
+  }
+
+  const taskCompleted = (id) => {
+    setToDoList(
+      toDoList.map((task) =>{
+        if(task.id === id){
+          return{...task, completed:true};
+        }else{
+          return task;
+        }
+      })
+    )
+  }
+
+  const deleteTask = (id) => {
+    setToDoList(toDoList.filter((task) => task.id !== id));
   }
    
   return (
@@ -25,7 +48,15 @@ function App() {
       </div>
       <div className='list'>
           {toDoList.map((task) => {
-            return <h1>{task}</h1>
+            return ( 
+              <Task 
+                  taskName={task.taskName} 
+                  id={task.id}
+                  completed={task.completed}
+                  deleteTask={deleteTask}
+                  taskCompleted={taskCompleted}
+              />
+            );
           })}
       </div>
     </div>
