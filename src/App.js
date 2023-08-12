3575fcb927f8a5a347c1d8b982e67ffb8e39a2c1
T-Luxshan <luckybraveboys@gmail.com>
2023-08-12 17:33:22 +0530

    input displayed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [toDoList, setToDoList] = useState([]); 
+  const [newTask, setNewTask] = useState(""); 
+
+  const handleChane = (event) => {
+    setNewTask(event.target.value);
+  };
+
+  const addTask = () => {
+    // const newToDoList = [...toDoList, newTask]
+    setToDoList([...toDoList, newTask])
+  }
+   
   return (
     <div className="App">
       {/* <header className="App-header">
-       
-        
-        
       </header> */}
+      <div className='addTask'>
+        <input onChange={handleChane}/>&nbsp;
+        <button onClick={addTask}>Add Task</button>
+      </div>
+      <div className='list'>
+          {toDoList.map((task) => {
+            return <h1>{task}</h1>
+          })}
+      </div>
     </div>
   );
 }
